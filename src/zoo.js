4b6a6c244a21b082217a68abc057aca3cc0d00c0
Gessé Carlos <gesse.dev@outlook.com>
2021-06-29 19:19:45 -0300

    feito ate o requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -1,30 +1,54 @@
 const data = require('./data');
 
+const { species } = data; // Importar o array species do arquivo data.js
+
+const { employees } = data; // Importar o array employees do arquivo data.js
+
 function getSpeciesByIds(ids) {
   // seu código aqui
+  if (!ids) return [];
+  const speciesIds = species.filter((specie) => ids.includes(specie.id));
+
+  return speciesIds;
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const getAnimalName = species.find((specie) => specie.name === animal);
+  const verifyAnimalAge = getAnimalName.residents.every((resident) => resident.age >= age);
+
+  return verifyAnimalAge;
 }
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  const emN = employees.find((em) => employeeName === em.firstName || employeeName === em.lastName);
+
+  return emN;
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
   // seu código aqui
+  const getManager = data.employees.find((employee) => employee.managers.includes(id));
+  if (getManager) return true;
+
+  return false;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  const setEmployee = data.employees.push({ id, firstName, lastName, managers, responsibleFor });
+
+  return setEmployee;
 }
 
-function countAnimals(species) {
+function countAnimals(speciesNames) { // O nome do parâmetro precisou ser mudado devido a linha 3
   // seu código aqui
 }
 
