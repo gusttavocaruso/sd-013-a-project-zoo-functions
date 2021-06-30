340311dce4cbdbf993ddef42b4a054c7ab6cbe8c
Julia Baptista <baptista.julia@yahoo.com.br>
2021-06-30 18:12:32 -0300

    Questao 4

--- src/zoo.js
+++ src/zoo.js
@@ -5,7 +5,7 @@ function getSpeciesByIds(...ids) {
   return ids.map((id) => species.find((specie) => specie.id === id));
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
@@ -16,7 +16,9 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return { id, firstName, lastName, managers, responsibleFor };
 }
 
 function isManager(id) {
