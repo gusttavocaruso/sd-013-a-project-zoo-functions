ecc97d550af3ed4b0acb66c1e4c93ef7a8afa488
sonkai <mfilho86@gmail.com>
2021-06-30 17:24:48 -0300

    requisito 1 feito a muito custo

--- src/zoo.js
+++ src/zoo.js
@@ -1,8 +1,19 @@
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui bora la
+function getSpeciesByIds(...ids) {
+  const def = [];
+  if (ids === undefined) return def;
+
+  const specsIds = [];
+
+  ids.forEach((id) => {
+    const fnd = data.species.find((newSpecsId) => newSpecsId.id === id);
+    specsIds.push(fnd);
+  });
+  return specsIds;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
