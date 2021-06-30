cbe473c492a0783f73b4d16a44232c7ceb98cb85
Gustavo Pia <piagja@gmail.com>
2021-06-30 16:20:02 -0300

    Atendeu ao requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -29,8 +29,14 @@ function getEmployeeByName(employeeName) {
     .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
 }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+function createEmployee({ id, firstName, lastName }, { managers, responsibleFor }) {
+  return {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
 }
 
 function isManager(id) {
