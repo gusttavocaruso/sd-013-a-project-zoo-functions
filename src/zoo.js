6273d92af8eb8d2ebf2dd8c9c9b2f3887667df0a
thomasravache <thomasravache31@gmail.com>
2021-06-30 18:25:20 -0300

    Adicionanod alteracoes

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { species } = require('./data');
+const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -16,7 +16,11 @@ function getAnimalsOlderThan(animal, age) {
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  return employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
+
+console.log(getEmployeeByName());
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
