73a29e598ffa65f0ba8ce2d3bffaa50e5b13ed55
Yuri Garrido <y.garrido.yuri@gmail.com>
2021-06-30 16:53:17 -0300

    envia requisito 05

--- src/zoo.js
+++ src/zoo.js
@@ -27,9 +27,12 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees
+    // .filter((pessoa, index) => pessoa.managers[index] === id)
+    .some((allPeoples, index) => allPeoples.managers[index] === id);
 }
 
+console.log(isManager(('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1')));
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
