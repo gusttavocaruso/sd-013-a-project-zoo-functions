10d74c34f00879e3d14229caf950daf3437af466
Rogerio P. Da Silva <rogeriop1990@gmail.com>
2021-06-30 19:28:11 -0300

    Refatorando Requisito 4 e Lint - Ok.

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,7 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
