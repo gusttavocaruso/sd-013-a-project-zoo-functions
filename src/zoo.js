5b6278ae0cc497225965666c7ea845d970cc8121
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-30 16:21:57 -0300

    Requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,8 @@
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter((specie, index) => specie.id === ids[index]);
 }
 
 function getAnimalsOlderThan(animal, age) {
