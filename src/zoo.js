fb77e7121618f8bd7dd74ad8cc53f2732b316b0b
Wallacy Francis <wallacy_francis@hotmail.com>
2021-06-30 15:49:59 -0300

    Resolvendo erros de lint para o requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -5,8 +5,6 @@ function getSpeciesByIds(...ids) {
   return species.filter((specie, index) => specie.id === ids[index]);
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
-
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
 }
