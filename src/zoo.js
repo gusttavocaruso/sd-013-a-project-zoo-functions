be960770336badb743096c396d20e18da58685e5
Julia Baptista <baptista.julia@yahoo.com.br>
2021-06-30 17:54:04 -0300

    Questao 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,8 +1,11 @@
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => species.find((specie) => specie.id === id));
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
