6ea79550dd189068252aa311d7f14ca9d99f1237
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-30 17:32:15 -0300

    Requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { species } = require('./data');
+const { species, employees } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -12,7 +12,9 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return employees.find((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName));
 }
 
 function createEmployee(personalInfo, associatedWith) {
