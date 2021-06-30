ee25df3a8078633cbb6658f49a68b3303b9fe0a3
junglejf <jfcchagas@id.uff.br>
2021-06-30 20:59:08 -0300

    Requisito 1. IMPLEMENTE A FUNÇÃO getSpeciesByIds-completo

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,9 @@
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  return (!ids.length ? [] : species.filter((specie) => ids.includes(specie.id)));
 }
 
 function getAnimalsOlderThan(animal, age) {
@@ -24,7 +26,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(speciesId) {
   // seu código aqui
 }
 
