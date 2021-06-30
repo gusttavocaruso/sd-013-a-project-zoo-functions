e22c1f4a2697c0c90147227c1b8712d066e60673
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-30 16:36:04 -0300

    Correção do lint

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(specie) {
   // seu código aqui
 }
 
