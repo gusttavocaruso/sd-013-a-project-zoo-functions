ed69a4c877aff890b37645a58600bd9ff54e7d96
Pedro Verano <pedroverano2009@yahoo.com.br>
2021-06-30 18:21:45 -0300

    Iniciando o projeto e adicionando requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,7 +1,10 @@
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (!ids) return [];
+  return ids.map((id) => data.species
+  .find((specie) => specie.id === id))
 }
 
 function getAnimalsOlderThan(animal, age) {
@@ -67,3 +70,5 @@ module.exports = {
   increasePrices,
   createEmployee,
 };
+
+console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274','e8481c1d-42ea-4610-8e11-1752cfc05a46'));
