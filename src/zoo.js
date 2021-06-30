db2065b5c2bf9599a3d09e0a65e762047769dcd9
Cássio Pereira <cassiorodp@gmail.com>
2021-06-30 16:43:33 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -27,6 +27,7 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
+  return employees.some(({ managers }) => managers.some((manager) => manager === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
