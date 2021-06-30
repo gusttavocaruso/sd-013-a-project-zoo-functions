64e35b9b93787fceee5b68590987053b0350bd0c
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-06-30 19:22:37 -0300

    requisito 5 - function is Manager

--- src/zoo.js
+++ src/zoo.js
@@ -29,7 +29,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((item) => item.managers.includes(id) === true);
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
