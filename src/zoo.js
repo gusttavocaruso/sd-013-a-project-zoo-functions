f2b599a02c846fff97373f5a6477a9d6f8b20aad
DouglasDrozda <lowdouglas9712@gmail.com>
2021-06-30 21:39:42 -0300

    feito requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -33,8 +33,9 @@ function isManager(id) {
   return data.employees.some((human) => human.managers.some((adult) => adult === id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
+  return data.employees.push(newEmployee);
 }
 
 function countAnimals(species) {
