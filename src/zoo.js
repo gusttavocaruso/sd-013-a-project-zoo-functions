0b898a36f479f6581d5753612c607777fbe1ce27
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-07-01 16:46:05 -0300

    desafio 2 feito

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,9 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((specie) => specie.name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
@@ -28,7 +30,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(_species) {
   // seu código aqui
 }
 
