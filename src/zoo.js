e9e8126911db376390bb82ceac857c572c0b9324
Caê <eduardokae01@hotmail.com>
2021-07-01 17:22:46 -0300

    linha 31 - substitui specie por especie para nao travar lint

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(especie) {
   // seu código aqui
 }
 
