71912e378aebe6227a68354593977dba67e591f6
PC <sthecso>
2021-07-01 15:00:33 -0300

    Requisito 3

--- test/getAnimalsOlderThan.test.js
+++ test/getAnimalsOlderThan.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const zoo = require('../src/zoo');
 
 describe('Implemente a função getAnimalsOlderThan', () => {
-  it('Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada', () => {
+  it('Ao passar o nome de uma espécie e uma idade, testa se  os animais desta espécie possuem a idade mínima especificada', () => {
     let actual = zoo.getAnimalsOlderThan('otters', 7);
     let expected = true;
     assert.deepStrictEqual(actual, expected);
