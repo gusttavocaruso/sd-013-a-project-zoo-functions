ff61e07ff2f49bb87bf21765d089aa66cd3a20b9
Erick Santos <erickosantos.dev@gmail.com>
2021-07-01 20:04:33 -0300

    Implemente a função getEmployeeByName

--- src/zoo.js
+++ src/zoo.js
@@ -1,4 +1,4 @@
-const { species: speciesData } = require('./data');
+const { species: speciesData, employees } = require('./data');
 
 function getSpeciesByIds(...ids) {
   return ids.map((id) => speciesData.find((specie) => specie.id === id));
@@ -10,7 +10,9 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
