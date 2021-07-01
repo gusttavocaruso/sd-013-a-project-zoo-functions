c86949518a09c64a6c9c3affb4d34c7bcb677707
LuizFJP <luizfjportela@gmail.com>
2021-07-01 16:33:42 -0300

    requirement 01

--- src/zoo.js
+++ src/zoo.js
@@ -2,30 +2,19 @@ const { species } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  if(ids.length === 0){
-    return [];
+  let animal = [];
+  if (ids.length === 0) {
+    return animal;
   }
-  else if(ids.length === 1){
-    const speciesId = species.filter(name =>
-    name.id === ids[0]);
-    return speciesId;
-  }
-  else{
-    let animal = [];
-    const speciesId = species.filter(name => {
-      ids.some(id => name === id)
-    });
-      animal.push(speciesId);
-
-  }
-  
+  animal = species.filter((specie) => ids.some((id) => specie.id === id));
+  return animal;
 }
 getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46');
 
 function getAnimalsOlderThan(animal, age) {
-  const animalElement = species.find(specie => specie.name === animal);
+  const animalElement = species.find((specie) => specie.name === animal);
   const result = animalElement.residents
-  .every(animalResident => animalResident.age >= age);
+    .every((animalResident) => animalResident.age >= age);
 
   return result;
 }
