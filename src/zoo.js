084506189617948eaf5a2a20c865869cdd89ce77
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-07-01 16:59:01 -0300

    Requisito 5,6,7,8,10

--- src/zoo.js
+++ src/zoo.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-const { species, employees } = require('./data');
+const { species, employees, prices, hours } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
@@ -32,19 +32,36 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers: managers || [],
+    responsibleFor: responsibleFor || [],
+  };
+  employees.push(newEmployee);
 }
 
-function countAnimals(speces) {
-  // seu código aqui
-}
+function countAnimals(animal) {
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  return species.find((animals) => animals.name === animal)
+    .residents.length;
+} 
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+  const calculate = (Adult * prices.Adult) + (Senior * prices.Senior) + (Child * prices.Child);
+  return calculate;
 }
 
 function getAnimalMap(options) {
@@ -52,8 +69,29 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  if (!dayName) {
+    return Object.entries(data.hours).reduce((acc, curr) => {
+      if (curr[0] === 'Monday') {
+        acc[curr[0]] = 'CLOSED';
+        return acc;
+      }
+      acc[curr[0]] = `Open from ${curr[1].open}am until ${curr[1].close - 12}pm`;
+      return acc;
+    }, {});
+  }
+  const findDay = Object.entries(data.hours).find((day) => day[0] === dayName);
+  const object = {};
+  if (dayName === 'Monday') {
+    object[findDay[0]] = 'CLOSED';
+    return object;
+  }
+  object[findDay[0]] = `Open from ${findDay[1].open}am until ${findDay[1].close - 12}pm`;
+  return object;
 }
+
+console.log(getSchedule());
+console.log(getSchedule('Monday'));
+  
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
