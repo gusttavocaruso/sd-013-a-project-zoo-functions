80c59aade0d2fbc8f81025cfd52661260abe26cd
Breno Lopes <brenokos4@gmail.com>
2021-07-02 06:57:07 -0300

    adiciona 5

--- src/zoo.js
+++ src/zoo.js
@@ -26,8 +26,14 @@ function createEmployee(personalInfo, associatedWith) {
   return newObj;
 }
 
-function isManager(id) {
-  // seu código aqui
+function isManager(idOfEmploy) {
+  const managers = [
+    '9e7d4524-363c-416a-8759-8aa7e50c0992',
+    'fdb2543b-5662-46a7-badc-93d960fdc0a8',
+    '0e7b460e-acf4-4e17-bcb3-ee472265db83',
+  ];
+  const idEmployer = data.employees.find(({ id }) => id === idOfEmploy);
+  return idEmployer.managers.includes(...managers);
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
