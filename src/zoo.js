14593a278139e80755d49a4374434099ab666710
Caê <eduardokae01@hotmail.com>
2021-07-02 19:07:05 -0300

    requisito 5 - isManager

--- src/zoo.js
+++ src/zoo.js
@@ -20,14 +20,14 @@ function getEmployeeByName(employeeName) {
   return employees.find((a) => a.firstName === employeeName || a.lastName === employeeName);
 }
 
-console.log(getEmployeeByName());
-
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
 }
 
 function isManager(id) {
-  // seu código aqui
+  const managers = ['9e7d4524-363c-416a-8759-8aa7e50c0992', 'fdb2543b-5662-46a7-badc-93d960fdc0a8', '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+  const test = managers.some((manager) => manager === id);
+  return test;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
