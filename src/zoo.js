cae9acfd43eda848ffa52917985829f8745aefe1
Mateus Montagnini <mateus.r.montagnini@gmail.com>
2021-07-02 17:05:54 -0300

    Resolve requisito 7 e 8

--- src/zoo.js
+++ src/zoo.js
@@ -18,37 +18,51 @@ function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  const employeeFullname = data.employees.find((employee) => // Não estava reconhecendo apenas o employees, então coloquei o data na frente. 
-    employee.firstName === employeeName || employee.lastName === employeeName); //Encontra o primeiro funcionário que tenha o primeiro ou último nome do parâmetro. 
+  const employeeFullname = data.employees.find((employee) =>// Não estava reconhecendo apenas o employees, então coloquei o data na frente. 
+    employee.firstName === employeeName || employee.lastName === employeeName);//Encontra o primeiro funcionário que tenha o primeiro ou último nome do parâmetro. 
   return employeeFullname;
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return {...personalInfo, ...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 function isManager(id) {
-  const managerEmployee = data.employees.some((employee) => employee.managers.some((manager) => manager === id)); /* Verifica se pelo menos um manager dentro de employees tem um id igual ao parametro e retorna true ou false. */
+  const managerEmployee = data.employees.some((employee) => employee.managers.some((manager) => manager === id)); 
+  /* Verifica se pelo menos um manager dentro de employees tem um id igual ao parametro e retorna true ou false. */
   return managerEmployee;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   const newEmployee = {
-    id: id,
-    firstName: firstName,
-    lastName: lastName,
+    id,
+    firstName,
+    lastName,
     managers: managers || [], // Para passar no teste que espera um array vazio.
     responsibleFor: responsibleFor || [], // Para passar no teste que espera um array vazio.
   };
-  return data.employees.push(newEmployee); // Coloca esse objeto do novo empregado no array "employees".
+  return data.employees.push(newEmployee); // Adiciona esse objeto do novo empregado no array "employees".
 }
 
 function countAnimals(species) {
-  // seu código aqui
+  if (species === undefined) {
+    const animals = {};
+    data.species.forEach((specie) => {animals[specie.name] = specie.residents.length;});
+    return animals;
+  }
+  return data.species.find((specie) => specie.name === species).residents.length;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined){
+     return 0; // Retorna 0 caso o parametro esteja vazio.
+  }
+
+  const adultPrice = data.prices.Adult * entrants.Adult || 0;
+  const seniorPrice = data.prices.Senior * entrants.Senior || 0;
+  const childPrice = data.prices.Child * entrants.Child || 0; /* Multiplica o numero de entradas de adultos, criancas e idosos pelo preco de cada entrada deles. Caso nao haja um valor para qualquer um desses, ele multiplica por zero. */
+
+  return adultPrice + seniorPrice + childPrice;
 }
 
 function getAnimalMap(options) {
