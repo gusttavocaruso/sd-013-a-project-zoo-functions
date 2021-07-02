c1dd8f46a0fd1675227a0edba6ff5037f245673b
Caê <eduardokae01@hotmail.com>
2021-07-02 19:55:39 -0300

    lint

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,11 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  const managers = ['9e7d4524-363c-416a-8759-8aa7e50c0992', 'fdb2543b-5662-46a7-badc-93d960fdc0a8', '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+  const managers = [
+    '9e7d4524-363c-416a-8759-8aa7e50c0992', 
+  'fdb2543b-5662-46a7-badc-93d960fdc0a8', 
+  '0e7b460e-acf4-4e17-bcb3-ee472265db83'
+];
   const test = managers.some((manager) => manager === id);
   return test;
 }
