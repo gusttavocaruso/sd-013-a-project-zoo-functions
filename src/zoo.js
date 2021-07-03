22bbdbb2ad51b3b704189e9e18d4165e0d648758
Lanai Conceição <conceicao.lanai@gmail.com>
2021-07-03 19:19:05 -0300

    Requisito 11

--- src/zoo.js
+++ src/zoo.js
@@ -20,8 +20,8 @@ function getSpeciesByIds(...ids) { // ...ids para receber mais de um parâmetro
 
 function getAnimalsOlderThan(animal, age) {
   return species
-    .find((specie) => specie.name === animal).residents
-    .every((resident) => resident.age >= age);
+    .find((specie) => specie.name === animal).residents // encontra as espécies e as iguala ao parâmetro digitado
+    .every((resident) => resident.age >= age); // depois verifica se todos os animais residentes tem a idade > idade do parâmetro
 }
 
 // ===========================
@@ -44,7 +44,7 @@ function getEmployeeByName(employeeName) {
 // ===========================
 
 function createEmployee(personalInfo, associatedWith) {
-  const { id, firstName, lastName } = personalInfo;
+  const { id, firstName, lastName } = personalInfo; // usando object destructuring para criar um parâmetro que recebe um objeto
   const { managers, responsibleFor } = associatedWith;
   return {
     id,
@@ -62,7 +62,7 @@ function createEmployee(personalInfo, associatedWith) {
 // ===========================
 
 function isManager(id) {
-  return employees.some((employee) => employee.managers.includes(id));
+  return employees.some((employee) => employee.managers.includes(id)); // verifica SE ALGUM dos ids do parâmetro é manager
 }
 
 // ===========================
@@ -71,7 +71,7 @@ function isManager(id) {
 
 // ===========================
 
-function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) { // predefinição de parâmetros, onde "managers" e "responsibleFor" ao não serem chamados como parâmetro devolvem um array vazio
   const objectAddEmployee = {
     id,
     firstName,
@@ -90,12 +90,12 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 
 function countAnimals(specie) {
   if (!specie) {
-    return species.reduce((acc, currentSpecie) => {
+    return species.reduce((acc, currentSpecie) => { // utilizando reduce() para transformar a função vazia em um retorno de obj vazio
       acc[currentSpecie.name] = currentSpecie.residents.length;
       return acc;
     }, {});
   }
-  return species.find((animal) => animal.name === specie).residents.length;
+  return species.find((animal) => animal.name === specie).residents.length; // indicando que o parâmetro é o nome do animal, e através do tamanho dele retornamos o número de residents daquela espécie
 }
 
 // ===========================
@@ -127,27 +127,30 @@ function getAnimalMap(options) {
 
 // ===========================
 
-function checkWeekDay(arrayEntries) {
-  return arrayEntries.reduce((acc, curr) => {
+// função que executa quando getSchedule() vem vazia
+function checkWeekDay(arrayEntries) { // quando a função principal recebe parâmetro vazio executa essa função aqui, que mostra na tela todos os horários, onde o único dia fechado é na segunda-feira
+  return arrayEntries.reduce((acc, curr) => { // reduzimos o retorno dessa função a CLOSED caso open e close sejam 0
     if (curr[1].open === 0 && curr[1].close === 0) {
-      acc[curr[0]] = 'CLOSED';
+      acc[curr[0]] = 'CLOSED'; // acumulador é o nome da chave que chama o dia da semana
       return acc;
     }
-    acc[curr[0]] = `Open from ${curr[1].open}am until ${curr[1].close - 12}pm`;
+    acc[curr[0]] = `Open from ${curr[1].open}am until ${curr[1].close - 12}pm`; // caso o open e close não sejam zero, retornamos todos os dias da semana e horários
     return acc;
   }, {});
 }
 
-function getSchedule(dayName) {
-  const arrayEntries = Object.entries(data.hours);
-  if (dayName === undefined) return checkWeekDay(arrayEntries);
+// função principal:
+
+function getSchedule(dayName) { // função principal do exercício (começar leitura do código aqui)
+  const arrayEntries = Object.entries(data.hours); // acessando o array que contém os horários
+  if (dayName === undefined) return checkWeekDay(arrayEntries); // se função vazia, vai para a função de cima
   const stringDay = arrayEntries.find((weekDay) => weekDay[0] === dayName); // se weekDay for igual ao dayName, retorna o array referente ao dia da semana
   const obj = {};
-  if (dayName === 'Monday') {
-    obj[stringDay[0]] = 'CLOSED';
+  if (dayName === 'Monday') { // se o parâmetro for segunda-feira
+    obj[stringDay[0]] = 'CLOSED'; // retornamos CLOSED
     return obj;
   }
-  obj[stringDay[0]] = `Open from ${stringDay[1].open}am until ${stringDay[1].close - 12}pm`;
+  obj[stringDay[0]] = `Open from ${stringDay[1].open}am until ${stringDay[1].close - 12}pm`; // se o parâmetro for qualquer outro dia da semana, retornamos o horário daquele dia
   return obj;
 }
 
@@ -158,7 +161,12 @@ function getSchedule(dayName) {
 // ===========================
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getFirstAnimal = employees.find((employee) => employee.id === id).responsibleFor[0]; // encontrando o primeiro animal que o funcionário é responsável por
+  const getFirstAnimalOfResidents = species
+    .find((specie) => specie.id === getFirstAnimal).residents; // fazendo link da primeira espécie de animal encontrado com os animais do objeto
+  getFirstAnimalOfResidents.sort((a, b) => b.age - a.age); // mostrando os animais em ordem decrescente para que o mais velho possa aparecer primeiro
+  const { name, sex, age } = getFirstAnimalOfResidents[0]; // usando Array Destructuring para criar um objeto
+  return [name, sex, age]; // transformando em array
 }
 
 // ===========================
