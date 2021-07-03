c3893fc39a2b3c6fb35f2140e3c20f2eac15d0f5
Gabriel Lenz <lenzordmusic@gmail.com>
2021-07-03 14:35:07 -0400

    Requisito 1 LINT

--- src/zoo.js
+++ src/zoo.js
@@ -4,59 +4,59 @@ function getSpeciesByIds(...rest) {
   const array = [];
   if (rest.length > 0) {
     rest.forEach((id) => {
-      let dadosSpecies = data.species.filter((objA) => objA.id === id)
+      const dadosSpecies = data.species.filter((objA) => objA.id === id);
       array.push(...dadosSpecies);
-    })
+    });
   }
   return array;
 }
 
 function getAnimalsOlderThan(animal, age) {
-  
+
 }
 
 function getEmployeeByName(employeeName) {
-  
+
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  
+
 }
 
 function isManager(id) {
-  
+
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  
+
 }
 
 function countAnimals(species) {
-  
+
 }
 
 function calculateEntry(entrants) {
-  
+
 }
 
 function getAnimalMap(options) {
-  
+
 }
 
 function getSchedule(dayName) {
-  
+
 }
 
 function getOldestFromFirstSpecies(id) {
-  
+
 }
 
 function increasePrices(percentage) {
-  
+
 }
 
 function getEmployeeCoverage(idOrName) {
-  
+
 }
 
 module.exports = {
