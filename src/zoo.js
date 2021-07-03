1f4ff650018df25e06122bf1dd4f1cdf94a8c46c
Matheus Camillo <matheuscamillo@hotmail.com.br>
2021-07-03 16:33:26 -0300

    resolvido desafio 6. addEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -33,8 +33,14 @@ function isManager(id) {
   return data.employees.some((each) => each.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers=[], responsibleFor=[]) {
+  const newEmployee = {id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(newEmployee);
 }
 
 function countAnimals(species) {
