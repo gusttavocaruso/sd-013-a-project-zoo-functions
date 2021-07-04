4a055b601728d94432f43605a6018e20102b219c
Tomas Santos <tomas.slvsts@gmail.com>
2021-07-04 17:27:27 -0300

    resoluções

--- src/zoo.js
+++ src/zoo.js
@@ -31,41 +31,127 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  const managersIds = employees.reduce(((acc, employee) => [...acc, ...employee.managers]), []);
+  const isManagerBool = managersIds.some((managerId) => managerId === id);
+
+  return isManagerBool;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  const employee = {
+    id,
+    firstName,
+    managers,
+    responsibleFor,
+  };
+
+  employees.push(employee);
 }
 
-function countAnimals(species) {
-  // seu código aqui
+function countAnimals(animalName) {
+  if (!animalName) {
+    return species.reduce(((acc, specie) => {
+      const { name } = specie;
+
+      return {
+        ...acc,
+        [name]: specie.residents.length,
+      };
+    }), {});
+  }
+
+  const quantity = species.find((specie) => specie.name === animalName).residents.length;
+
+  return quantity;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
-}
+  if (!entrants) {
+    return 0;
+  }
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+  const {
+    Adult = 0,
+    Senior = 0,
+    Child = 0,
+  } = entrants;
 
+  const {
+    Adult: adultPrice,
+    Senior: seniorPrice,
+    Child: childPrice,
+  } = prices;
+
+  return (Child * childPrice + Senior * seniorPrice + Adult * adultPrice);
+}
+function getAnimalMap(options = {}) {
+  const animalsByLocation = {
+    NE: filterAnimals('NE', options),
+    SW: filterAnimals('SW', options),
+    SE: filterAnimals('SE', options),
+    NW: filterAnimals('NW', options),
+  };
+
+  return animalsByLocation;
+}
 function getSchedule(dayName) {
-  // seu código aqui
+  if (!dayName) {
+    const days = Object.keys(hours);
+    return days.reduce((acc, day) => {
+      if (day === 'Monday') {
+        return { ...acc, [day]: 'CLOSED' };
+      }
+      const openCloseMsg = `Open from ${hours[day].open}am until ${hours[day].close - 12}pm`;
+      return { ...acc, [day]: openCloseMsg };
+    }, {});
+  }
+
+  return dayName === 'Monday' ? { [dayName]: 'CLOSED' }
+  : { [dayName]: `Open from ${hours[dayName].open}am until ${hours[dayName].close - 12}pm` };
 }
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = employees.find((emp) => emp.id === id);
+  const firstAnimal = species.find((specie) => specie.id === employee.responsibleFor[0]);
+  const oldestAnimal = firstAnimal.residents.reduce((acc, resident) => (resident.age > acc.age ? resident : acc));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const pricesKeys = Object.keys(prices);
+  pricesKeys.forEach((key) => {
+    const price = prices[key];
+    prices[key] = Math.ceil((price + (price * (percentage / 100))) * 100) / 100;
+  });
 }
+
+const getAnimalsOfEmployee = (animalsId) => animalsId
+.reduce((acc, id) => {
+  const animalsName = species.filter((specie) => specie.id === id)
+    .reduce(((acc2, specie) => acc2 + specie.name), []);
+  return [...acc, animalsName];
+}, []);
 
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
+  if (!idOrName) {
+    return employees.reduce((acc, employee) => {
+      const { firstName, lastName, responsibleFor } = employee;
+      const fullName = `${firstName} ${lastName}`;
+      const animalsOfEmployee = getAnimalsOfEmployee(responsibleFor);
+      return {
+        ...acc,
+        [fullName]: animalsOfEmployee,
+      };
+    }, {});
+  }
+  const { firstName, lastName, responsibleFor } = employees.find((employee) =>
+    employee.firstName === idOrName
+    || employee.lastName === idOrName
+    || employee.id === idOrName);
+  const fullName = `${firstName} ${lastName}`;
+  const animalsOfEmployee = getAnimalsOfEmployee(responsibleFor);
+  return { [fullName]: animalsOfEmployee };
 }
-
 module.exports = {
   calculateEntry,
   getSchedule,
