426b68c1acf05a784ca3fdfb218b8ed27b12ef0f
Silvio Fabian <silviofabian334@gmail.com>
2021-07-04 20:32:26 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((item) => item.managers.includes(id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
