6c94829514823bc60a6dd05f1eb2ae0d559fe201
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-04 14:06:20 -0300

    Adiciona resolução do requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -6,7 +6,8 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getSpecie = data.species.find((specie) => specie.name === animal);
+  return getSpecie.residents.every((resident) => resident.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
