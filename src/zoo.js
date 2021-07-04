e0b23d6b282794a0783600436a53e24c31001002
Olavio Timoteo <olaviotimoteo@hotmail.com>
2021-07-04 20:23:10 -0300

    Adiciona resolução do requisito 8

--- src/zoo.js
+++ src/zoo.js
@@ -56,7 +56,11 @@ function countAnimals(species) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+  return Adult * data.prices.Adult + Senior * data.prices.Senior + Child * data.prices.Child;
 }
 
 function getAnimalMap(options) {
