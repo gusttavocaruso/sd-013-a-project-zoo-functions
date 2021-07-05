10ac273833d718d943d134f472d4c1c516cedbee
Bruno Pinho <brunopinho@outlook.com>
2021-07-05 16:13:30 -0300

    feito requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,8 +1,12 @@
+const { species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(firstId, secondId) {
+  let getSpecies = species.filter(specie => specie.id === firstId || specie.id === secondId);
+  return getSpecies;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
