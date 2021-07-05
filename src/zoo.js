adf0cd24bbab615dc47d9db37903969dbf07056f
Matheus Duarte de Freitas <freickup@gmail.com>
2021-07-04 21:36:53 -0300

    exercico 1 finalizado

--- src/zoo.js
+++ src/zoo.js
@@ -1,8 +1,10 @@
-const data = require('./data');
+const { species, employees, prices } = require('./data');// Atualizo o require com os arrays que quero obter do arquivo data.
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return species.filter(({ id }) => ids.includes(id));// Pego o array species e filtro ele de forma que em cada iteração por um elemento-objeto-especie eu pego a propriedade "id" e comparo se ela é igual a algum dos parametros passados em "...ids", se for, este elemento-objeto-animal entra no array que está sendo montado na funcao, ao final, a funcao retorna este array.
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
@@ -24,7 +26,7 @@ function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
 
-function countAnimals(species) {
+function countAnimals(especies) {
   // seu código aqui
 }
 
