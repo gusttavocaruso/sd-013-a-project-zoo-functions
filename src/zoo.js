d0cfddc8cb38acd73193fc1e850cbe6745106518
Isabella Antunes <bella.antunes2@gmail.com>
2021-07-05 20:43:47 -0300

    Requisito 12

--- src/zoo.js
+++ src/zoo.js
@@ -104,7 +104,9 @@ function getOldestFromFirstSpecies(id) {
 
 // REQUISITO 12 _______________________________________________________________________
 function increasePrices(percentage) {
-  // seu código aqui
+  prices.Adult = Math.round(prices.Adult * (1 + percentage / 100) * 100) / 100;
+  prices.Senior = Math.round(prices.Senior * (1 + percentage / 100) * 100) / 100;
+  prices.Child = Math.round(prices.Child * (1 + percentage / 100) * 100) / 100;
 }
 
 // REQUISITO 13 _______________________________________________________________________
