adfcb9b9a9edcf1ec20ddd1f8bc38a11ff8b52c2
suco <francalucas937@gmail.com>
2021-07-05 19:14:16 -0300

    localizando animais de maior idade pelo cuidador

--- src/zoo.js
+++ src/zoo.js
@@ -108,7 +108,7 @@ function getSchedule(dayName) {
 }
 
 function getOldestFromFirstSpecies(id) {
-  // const findEmployee = data.employees.find((employee) => employee.id === id);
+  const findEmployee = data.employees.find((employee) => employee.id === id);
   const animalFinder = data.species.find((specie) => id.responsibleFor[0] === specie.id);
   const sortAnimalF = animalFinder.residents.sort((a, b) => b.age - a.age)[0];
   return [sortAnimalF.name, sortAnimalF.sex, sortAnimalF.age];
