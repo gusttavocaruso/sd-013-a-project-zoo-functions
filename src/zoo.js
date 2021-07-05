cd708547cb2bab5c0d72ee0afebee1478c293380
Bruno Pinho <brunopinho@outlook.com>
2021-07-05 16:35:32 -0300

    iniciado requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -6,11 +6,15 @@ function getSpeciesByIds(firstId, secondId) {
   return getSpecies;
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const checkAnimalsAge = species
+  .filter(specie => specie.name === animal)
+  .every(specie => specie.residents.age >= age);
+
+  return checkAnimalsAge;
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
