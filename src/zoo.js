db716d89f8f5704f80357c6c555e24c16b1e4f39
Murilo Rainho <murilorainho01@gmail.com>
2021-07-05 20:01:47 -0300

    Quase todos os requisitos finalizados, faltando apenas o 9 e o 12

--- src/zoo.js
+++ src/zoo.js
@@ -55,28 +55,75 @@ function calculateEntry({ Adult = 0, Child = 0, Senior = 0 } = 0) {
   return (Adult * Ad) + (Senior * Se) + (Child * Ch);
 }
 
-function getAnimalMap(options) {
-  const loc = data.species.reduce((accumulator, current) => {
-    const local = accumulator;
-    local[current.location] = push(current.name);
+function getAnimalMap({ includeNames = false, sorted = false, sex = false } = false) {
+  const mapAnimals = data.species.reduce((acc, crt) => {
+    if (acc[crt.location] !== undefined) {
+      acc[crt.location].push(crt.name);
+    } else {
+      acc[crt.location] = [];
+      acc[crt.location].push(crt.name);
+    }
+    return acc;
   }, {});
-  return loc;
+  return mapAnimals;
 }
+// if (includeNames = true) {
+//   Object.keys(mapAnimals).forEach((element) => {
+//     mapAnimals[element] = mapAnimals[element].map((curr) => {
+//       const sectionAnimal = data.species.find((element) => element.name === curr);
+//       const arr = sectionAnimal.residents.map((nam) => `${nam.name}`);
+//       return { [curr]: arr };
+//     });
+//   });
+// }
+
+// console.log(getAnimalMap({ includeNames: true }));
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const dataHour = { ...data.hours };
+  Object.entries(dataHour).forEach((arr) => {
+    dataHour[arr[0]] = `Open from ${arr[1].open}am until ${arr[1].close - 12}pm`;
+    if (arr[1].open === arr[1].close) {
+      dataHour[arr[0]] = 'CLOSED';
+    }
+  });
+  return (dayName !== undefined) ? { [dayName]: dataHour[dayName] } : dataHour;
 }
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const person = data.employees.find((employee) => employee.id === id);
+  const animals = data.species.find((animal) => animal.id === person.responsibleFor[0]);
+  const oldest = animals.residents.reduce((old, curr) => ((old.age < curr.age) ? curr : old));
+  return [oldest.name, oldest.sex, oldest.age];
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  Object.keys(data.prices).forEach((element) => {
+    data.prices[element] += (data.prices[element] * (percentage / 100));
+    data.prices[element] = parseFloat(data.prices[element].toFixed(2));
+  });
+  return data.prices;
 }
 
+// console.log(increasePrices(50));
+// console.log(increasePrices(30));
+
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
+  const employees = data.employees.reduce((acc, curr) => {
+    const arr = curr.responsibleFor.map((element) => {
+      const pet = data.species.find((animal) => animal.id === element);
+      return pet.name;
+    });
+    acc[`${curr.firstName} ${curr.lastName}`] = arr;
+    return acc;
+  }, {});
+  if (idOrName !== undefined) {
+    let employee = data.employees.find((worker) => worker.id === idOrName);
+    if (employee === undefined) employee = getEmployeeByName(idOrName);
+    const tempStr = `${employee.firstName} ${employee.lastName}`;
+    return { [tempStr]: employees[tempStr] };
+  }
+  return employees;
 }
 
 module.exports = {
