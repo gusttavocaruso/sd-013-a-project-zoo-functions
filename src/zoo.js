465bf58494370791899599f6ec81e1a995f07b54
Emídio Rebouças <emidioreb@gmail.com>
2021-07-05 19:19:23 -0300

    Realização do requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -51,9 +51,38 @@ function getEmployeeByName(employeeName) {
 })
 }
 
+/*---------------------------------------------------------------------------------------------------*/
+
+/* ------------------------------------4°Requisito--------------------------------------------------*/
+/* Referência: https://pt.stackoverflow.com/questions/215685/juntar-dois-jsons-em-um-%C3%BAnico-objeto */
+/* Qual o objetivo dessa função? 
+  - Criar um objeto equivalente ao de uma pessoa colaboradora.
+  - O parâmetro personalInfo recebe um objeto que contém o id, o firstName e o lastName
+  - O parâmetro associatedWith recebe um objeto que contém dois array: managers e responsibleFor
+*/
+/*O que será avaliado ?
+  - Cria um novo colaborador a partir de objetos contendo informações pessoais e gerentes e animais gerenciados. */
+
+
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return Object.assign({},personalInfo,associatedWith)
+  
 }
+console.log(createEmployee({
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+},{
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+}));
 
 function isManager(id) {
   // seu código aqui
