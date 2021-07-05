c2d43d64414b0bffa0505abf2ab101a6476fa02d
Micael Santos <micaelmaicon@hotmail.com>
2021-07-05 19:12:29 -0300

    Finish requirement 1

--- src/zoo.js
+++ src/zoo.js
@@ -1,11 +1,14 @@
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  const especies = data.species.filter((id) => id.id === ids);
-  console.log(especies);
+  if (!ids) return []; // Se a função for chamada sem parâmetro...
+  const speciesReturn = [];
+  ids.filter((id) => data.species.forEach((idSpecies) => {
+    if (idSpecies.id === id) speciesReturn.push(idSpecies);
+  }));
+  return speciesReturn;
 }
-getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
