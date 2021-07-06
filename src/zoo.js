3109bcf084f1b4f24b0176ff641042df67934e29
Emídio Rebouças <emidioreb@gmail.com>
2021-07-06 10:37:38 -0300

    Realização do requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -68,25 +68,43 @@ function createEmployee(personalInfo, associatedWith) {
   return Object.assign({},personalInfo,associatedWith)
   
 }
-console.log(createEmployee({
-  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
-  firstName: 'John',
-  lastName: 'Doe',
-},{
-  managers: [
-    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
-    '9e7d4524-363c-416a-8759-8aa7e50c0992'
-  ],
-  responsibleFor: [
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
-    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
-  ]
-}));
+// console.log(createEmployee({
+//   id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+//   firstName: 'John',
+//   lastName: 'Doe',
+// },{
+//   managers: [
+//     'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//     '9e7d4524-363c-416a-8759-8aa7e50c0992'
+//   ],
+//   responsibleFor: [
+//     '0938aa23-f153-4937-9f88-4858b24d6bce',
+//     '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+//     'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+//   ]
+// }));
 
+/*---------------------------------------------------------------------------------------------------*/
+
+/* ------------------------------------5°Requisito--------------------------------------------------*/
+/* Qual o objetivo dessa função? 
+  - Verifica se uma pessoa colaboradora, a partir de seu id, ocupa cargo de gerência.
+*/
+/*O que será avaliado ?
+  - Testa se o id passado é de um gerente */
+// function isManager(id) {
+//   return employees.find((employee) => employee.id === id).managers
+//   .some((manager) => manager === 'stephanieId' || manager === undefined)
+  
+// }
+// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 function isManager(id) {
-  // seu código aqui
-}
+return employees.some((employee) => employee.managers.some((manager) => manager === id))}
+  
+console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+
+
+
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
