2889389f9915064d10149708252aec0139527c4c
Rodrigo Pova <rodrigopova94@gmail.com>
2021-07-06 01:27:16 -0300

    Resolve requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -24,8 +24,9 @@ function isManager(id) {
   return employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
+  employees.push(newEmployee);
 }
 
 function countAnimals(especie) {
