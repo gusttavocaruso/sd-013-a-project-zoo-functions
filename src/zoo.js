4d064c31eb8e7b99f4f7e7a8c26ae29e931ecf3f
igor <igorhdsouza@gmail.com>
2021-07-05 21:40:56 -0300

    requisito 03

--- src/zoo.js
+++ src/zoo.js
@@ -15,6 +15,9 @@ function getAnimalsOlderThan(animal, age) {
 }
 
 function getEmployeeByName(employeeName) {
+  if (!employeeName) return {};
+  return data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
