fc3caf77906305595f78d43b3330e8d7a1833448
Eduardomak <eduardomak92@gmail.com>
2021-07-09 21:16:34 -0300

    requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -66,9 +66,21 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const time = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  const objectDay = time[`${dayName}`];
+  const result = {};
+  result[`${dayName}`] = objectDay;
+  return Object.keys(time).includes(dayName) === true ? result : time;
 }
-
+console.log(getSchedule('Tuesday'));
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
 }
