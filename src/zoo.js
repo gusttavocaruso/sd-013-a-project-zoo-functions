9e975145babd1a81061e375fe9fb8c29333e455a
Sethano <vitorcard1720@gmail.com>
2021-07-09 23:42:54 -0300

    comecei finalmente

--- src/zoo.js
+++ src/zoo.js
@@ -1,23 +1,43 @@
 const data = require('./data');
 // comecando o projeto
-function getSpeciesByIds(ids) {
-  // seu código aqui
+
+function getSpeciesByIds(...ids) {
+  const blankArr = [];
+  if (ids === null) {
+    return blankArr;
+  }
+  const speciesArr = data.species.filter((specie) => ids.includes(specie.id));
+  return speciesArr;
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const specieToAge = data.species.filter((specie) => specie.name === animal);
+  const minimalAge = specieToAge[0].residents.find((resident) => resident.age < age);
+  return !minimalAge;
 }
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(name) {
+  const blankObj = {};
+  if (name === undefined) {
+    return blankObj;
+  }
+  const epy = data.employees.filter((e) => name === e.firstName || name === e.lastName);
+  return epy[0];
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmploye = {
+    ...personalInfo,
+    ...associatedWith,
+  };
+  return newEmploye;
 }
 
 function isManager(id) {
-  // seu código aqui
+  const manager = [];
+  data.employees.forEach((manajj) => manager.push(...manajj.managers));
+  const result = manager.includes(id);
+  return result;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
