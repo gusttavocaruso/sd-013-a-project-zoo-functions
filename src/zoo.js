3ec972b02df293d8c9053cde2262f8034e4ff379
Pablo Almeida <pabloalmeidac@hotmail.com>
2021-07-11 17:46:40 -0300

    requisito 4,5 e 6 concludo

--- src/zoo.js
+++ src/zoo.js
@@ -32,8 +32,8 @@ function isManager(id) {
   return data.employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
 }
 
 function countAnimals(speciesCount) {
