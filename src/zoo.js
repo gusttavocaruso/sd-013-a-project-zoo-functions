a1a5adda6bf91cdcf0344110cea679b6c34db0f6
Cassyel <cassyel1967@gmail.com>
2021-07-11 17:24:46 -0300

    Realiza requisitos

--- src/zoo.js
+++ src/zoo.js
@@ -1,12 +1,19 @@
-const { employees } = require('./data');
+const { employees, species } = require('./data');
 const data = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const result = [];
+
+  ids.forEach((id) => {
+    result.push(species.find((specie) => specie.id === id));
+  });
+
+  return result;
 }
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((specie) => specie.name === animal)
+    .residents.every((item) => item.age > age);
 }
 
 function getEmployeeByName(employeeName) {
@@ -18,23 +25,52 @@ function getEmployeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployee = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return newEmployee;
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employ) => (employ.managers.includes(id)));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  return employees.push(newEmployee);
 }
 
-function countAnimals(species) {
-  // seu código aqui
+function countAnimals(animais) {
+  const animals = {};
+  if (animais === undefined) {
+    species.forEach((specie) => { animals[specie.name] = specie.residents.length; });
+    return animals;
+  } return species.find((specie) => specie.name === animais).residents.length;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let result = 0;
+
+  if (entrants === undefined) {
+    return result;
+  } if (entrants.Adult) {
+    result += entrants.Adult * 49.99;
+  } if (entrants.Senior) {
+    result += entrants.Senior * 24.99;
+  } if (entrants.Child) {
+    result += entrants.Child * 20.99;
+  }
+  return result;
 }
 
 function getAnimalMap(options) {
@@ -50,11 +86,31 @@ function getOldestFromFirstSpecies(id) {
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const porcentagem = (percentage / 100) + 1;
+
+  Object.keys(data.prices).forEach((person) => {
+    data.prices[person] = Math.round((data.prices[person] * porcentagem * 100)) / 100;
+  });
 }
 
 function getEmployeeCoverage(idOrName) {
-  // seu código aqui
+  const fullName = (person) => `${person.firstName} ${person.lastName}`;
+  const verifyIdOrName = (person) => (person.firstName === idOrName)
+  || (person.lastName === idOrName) || (person.id === idOrName);
+  const findName = (arrayOfIds) => {
+    const specieName = [];
+    arrayOfIds.forEach((id) => specieName
+      .push(data.species.find(((specie) => specie.id === id)).name));
+    return specieName;
+  };
+  const employeesList = data.employees
+    .reduce((acc, person) => ({ ...acc, [fullName(person)]: findName(person.responsibleFor) }), {});
+
+  if (!idOrName) return employeesList;
+  const result = data.employees
+    .find((employee) => verifyIdOrName(employee));
+
+  return { [fullName(result)]: findName(result.responsibleFor) };
 }
 
 module.exports = {
