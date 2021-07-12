71dbeba3c8a21f41290060963c84f4ade6e038b9
ggreg1987 <gabriel.gregorio1987@gmail.com>
2021-07-12 02:35:06 -0300

    Desafio 8

--- src/zoo.js
+++ src/zoo.js
@@ -46,7 +46,11 @@ function countAnimals(specieName) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+  return Adult * data.prices.Adult + Senior * data.prices.Senior + Child * data.prices.Child;
 }
 
 function getAnimalMap(options) {
