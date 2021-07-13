f405eeb78f3f2ce21f3f72beff4d2b80735013d8
Amaury Queiroz Jr <amaury.tqj@gmail.com>
2021-07-13 15:34:25 -0300

    resolvendo function isManager

--- src/zoo.js
+++ src/zoo.js
@@ -22,7 +22,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.find((manager) => manager === id));
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
