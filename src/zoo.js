1ad08b4fe638bcee636cc71882dca78739a4135a
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-21 02:06:45 -0300

    Requisito 7

--- src/zoo.js
+++ src/zoo.js
@@ -43,7 +43,13 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 }
 
 function countAnimals(specie) {
-  // seu código aqui
+  if (!specie) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  return species.find((animal) => animal.name === specie).residents.length;
 }
 
 function calculateEntry(entrants) {
