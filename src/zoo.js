65c268e9c4bb76256ff0a6ea0d6589912b56bb02
João Victor Paduam <joao.paduam@hotmail.com>
2021-07-24 21:33:43 -0300

    Finalizacao do requisito 11 e 12

--- src/zoo.js
+++ src/zoo.js
@@ -1,6 +1,6 @@
 const data = require('./data');
 
-const { species, employees, prices } = data;
+const { species, employees, prices, hours } = data;
 
 const getSpeciesByIds = (...ids) => species.filter((specie) => ids.includes(specie.id));
 
@@ -48,15 +48,28 @@ function getAnimalMap(options) {
 }
 
 function getSchedule(dayName) {
-  // seu código aqui
+  const keyHours = Object.keys(hours);
+  const valueHours = Object.values(hours);
+  const openHours = valueHours.map((h) => h.open);
+  openHours.find((hour) => hour === 0);
+  console.log(openHours);
 }
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(funcID) {
+  const funcionario = employees.find(({ id }) => id === funcID);
+  const especie = funcionario.responsibleFor[0];
+  const animals = species.find(({ id }) => id === especie);
+  const olderAnimal = animals.residents.reduce((acc, a) => (acc.age < a.age ? a : acc));
+  const final = Object.values(olderAnimal);
+  return final;
 }
+getOldestFromFirstSpecies('4b40a139-d4dc-4f09-822d-ec25e819a5ad');
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const multi = (100 + percentage) / 100;
+  prices.Adult = Math.round((prices.Adult * multi) * 100) / 100;
+  prices.Senior = Math.round((prices.Senior * multi) * 100) / 100;
+  prices.Child = Math.round((prices.Child * multi) * 100) / 100;
 }
 
 function getEmployeeCoverage(idOrName) {
