acd5ffee615e2acdc9cca36c70a7d590648cf031
Danilo Uehara <daniloyuno@hotmail.com>
2021-07-25 22:53:59 -0300

    add alguns exerc

--- src/zoo.js
+++ src/zoo.js
@@ -1,36 +1,85 @@
-const data = require('./data');
+const data = require("./data");
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  test
+  const animals = [];
+  ids.forEach((id) => {
+    const especies = data.species.find((species) => species.id === id);
+    animals.push(especies);
+  });
+  return animals;
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const especies = data.species.find((specie) => specie.name === animal);
+  return especies.residents.every((idade) => idade.age >= age);
 }
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find(
+    (funcionario) =>
+      funcionario.firstName === employeeName ||
+      funcionario.lastName === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const merge = { ...personalInfo, ...associatedWith };
+  return merge;
 }
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((gerente) => gerente.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(
+  id,
+  firstName,
+  lastName,
+  managers = [],
+  responsibleFor = []
+) {
   // seu código aqui
+  const novoFuncionario = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  return data.employees.push(novoFuncionario);
 }
 
 function countAnimals(species) {
   // seu código aqui
+  if (species === undefined) {
+    return data.species.reduce((acumulador, valorAtual) => {
+      const count = acumulador;
+      count[valorAtual.name] = valorAtual.residents.length;
+      return count;
+    }, {});
+  }
+  return data.species.find((specie) => specie.name === species).residents.length;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || entrants === {}) {
+    return 0;
+  }
+  const {Adult = 0, Child = 0, Senior = 0 } = entrants;
+  const price =
+    Adult * data.prices.Adult +
+    Child * data.prices.Child +
+    Senior * data.prices.Senior;
+  return price;
 }
 
 function getAnimalMap(options) {
@@ -39,6 +88,7 @@ function getAnimalMap(options) {
 
 function getSchedule(dayName) {
   // seu código aqui
+  
 }
 
 function getOldestFromFirstSpecies(id) {
@@ -47,6 +97,7 @@ function getOldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   // seu código aqui
+  
 }
 
 function getEmployeeCoverage(idOrName) {
