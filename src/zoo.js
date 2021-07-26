48dcf7ede74517f6ce993785eab27de160e9783d
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-07-26 18:30:28 -0300

    requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -7,6 +7,8 @@ function getSpeciesByIds(ids) {
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animals = data.species.find((specie) => specie.name === animal);
+  return animals.residents.every((resident) => resident.age > age);
 }
 
 function getEmployeeByName(employeeName) {
