6b5bdb5c50e95b6ab6a6dee5d003776d70a2c7ea
Xenon-Neko <darkankher@gmail.com>
2021-07-27 13:03:44 -0300

    Projeto zoo functions v13

--- src/zoo.js
+++ src/zoo.js
@@ -17,7 +17,8 @@ function getSpeciesByIds(...ids) {
 }
 
 function getAnimalsOlderThan(animal, age) {
-  return data.species.find((allAnimals) => allAnimals.name === animal).residents.every((resident) => resident.age > age);
+  return data.species.find((allAnimals) => {
+  allAnimals.name === animal}).residents.every((resident) => resident.age > age);
 }
 
 function getEmployeeByName(employeeName) {
