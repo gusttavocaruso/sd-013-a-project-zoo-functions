0aa8925ea59344426c6f2be437cc9245d9176e63
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-07-26 22:54:59 -0300

    requisitos 7, 8 e 10

--- src/zoo.js
+++ src/zoo.js
@@ -1,13 +1,14 @@
+const { employees, prices } = require('./data');
+const { species } = require('./data');
 const data = require('./data');
 
 function getSpeciesByIds(...ids) {
-  const animalsId = data.species;
-  return animalsId.filter((specie) => ids.includes(specie.id));
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const animals = data.species.find((specie) => specie.name === animal);
+  const animals = species.find((specie) => specie.name === animal);
   return animals.residents.every((resident) => resident.age > age);
 }
 
@@ -16,7 +17,7 @@ function getEmployeeByName(employeeName) {
   if (employeeName === undefined) {
     return {};
   }
-  return data.employees.find((employee) =>
+  return employees.find((employee) =>
     employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
@@ -27,14 +28,13 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   // seu código aqui
-  const employeeList = data.employees;
-  return employeeList.some((employee) => employee.managers.includes(id));
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 // seu código aqui
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-  data.employees.push({
+  employees.push({
     id,
     firstName,
     lastName,
@@ -43,21 +43,50 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   });
 }
 
-function countAnimals(species) {
-  // seu código aqui
-
+// function countAnimals(species) {
+// seu código aqui
+function countAnimals(specie) {
+  const allAnimals = {};
+  if (specie === undefined) {
+    species.forEach((animalSpecie) => {
+      allAnimals[animalSpecie.name] = animalSpecie.residents.length;
+    });
+    return allAnimals;
+  }
+  const animals = species.find((animalSpecie) => animalSpecie.name === specie);
+  return animals.residents.length;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (entrants === undefined || entrants === {}) {
+    return 0;
+  }
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+  const adult = prices.Adult * Adult;
+  const child = prices.Child * Child;
+  const senior = prices.Senior * Senior;
+  return adult + child + senior;
 }
-
 function getAnimalMap(options) {
   // seu código aqui
 }
 
 function getSchedule(dayName) {
   // seu código aqui
+  const days = {
+    Tuesday: 'Open from 8am until 6pm',
+    Wednesday: 'Open from 8am until 6pm',
+    Thursday: 'Open from 10am until 8pm',
+    Friday: 'Open from 10am until 8pm',
+    Saturday: 'Open from 8am until 10pm',
+    Sunday: 'Open from 8am until 8pm',
+    Monday: 'CLOSED',
+  };
+  if (dayName === undefined) {
+    return days;
+  }
+  return { [dayName]: days[dayName] };
 }
 
 function getOldestFromFirstSpecies(id) {
